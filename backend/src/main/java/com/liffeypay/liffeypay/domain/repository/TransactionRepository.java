0bd69004b4f5953d5224dc8120bc11469c968885
kruzvinicius <kruzvinicius@proton.me>
2026-04-28 08:43:15 -0300

    feat: add findByTargetWalletIdAndType and findBySourceWalletIdAndType to TransactionRepository

--- backend/src/main/java/com/liffeypay/liffeypay/domain/repository/TransactionRepository.java
+++ backend/src/main/java/com/liffeypay/liffeypay/domain/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.liffeypay.liffeypay.domain.repository;
 
 import com.liffeypay.liffeypay.domain.model.Transaction;
+import com.liffeypay.liffeypay.domain.model.TransactionType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
     Page<Transaction> findAllBySourceWalletIdOrTargetWalletId(UUID sourceWalletId, UUID targetWalletId, Pageable pageable);
+    Page<Transaction> findByTargetWalletIdAndType(UUID targetWalletId, TransactionType type, Pageable pageable);
+    Page<Transaction> findBySourceWalletIdAndType(UUID sourceWalletId, TransactionType type, Pageable pageable);
 }
